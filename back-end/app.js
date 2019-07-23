cdd64771eaae0528e7eb5fa4cf22b64c4e12f177
MrPotato98 <huy.potato1998@gmail.com>
2019-07-23 16:25:35 +0700

    aaa

--- back-end/app.js
+++ back-end/app.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const app =express ();
-const mongoose = require ('mongoose');
-const bodyParser= require ('body-parser')
-require ("dotenv/config");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv/config");
 
 //Routes
-app.use (bodyParser.json());
+app.use(bodyParser.json());
 
-const postRoute = require ('./routes/post');
-app.use ("/post", postRoute);
-
+const postRoute = require("./routes/post");
+app.use("/post", postRoute);
 
 //Connect Db
-mongoose.connect (process.env.DB_CONNECTION,{ useNewUrlParser: true },(err, success)=>{
-    if (err)
-    {
-        console.log (err)
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
+  (err, success) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected mongodb huy");
     }
-    else
-    {
-        console.log("Connected mongodb");
-    }
-    
-});
+  }
+);
 
 app.listen(3000);
