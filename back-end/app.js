a1051689431c072159bbdf7cb03f2451d517abcf
Quan <hq15ht@gmail.com>
2019-07-31 23:23:36 +0700

    sss

--- back-end/app.js
+++ back-end/app.js
@@ -5,31 +5,17 @@ const bodyParser = require("body-parser");
 require("dotenv/config");
 
 //Routes
-<<<<<<< HEAD
 app.use (bodyParser.json());
 
 const postRoute = require ('./routes/user');
 app.use ("/user", postRoute);
-=======
-app.use(bodyParser.json());
->>>>>>> e034cf9ff92f6f29c77445602407e7703bcafedf
 
-const postRoute = require("./routes/post");
-app.use("/post", postRoute);
+app.use(bodyParser.json());
+
+
 
 //Connect Db
-mongoose.connect(
-  process.env.DB_CONNECTION,
-  { useNewUrlParser: true },
-  (err, success) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Connected mongodb huy");
-    }
-<<<<<<< HEAD
-    
-});
+
 // DB_CONNECTION=mongodb+srv://quansean:150598bd!@tesa-e6rwo.mongodb.net/test?retryWrites=true&w=majority
 
 app.listen(3000,(err, success)=>{
@@ -39,13 +25,10 @@ app.listen(3000,(err, success)=>{
     }
     else 
     {
-        console.log ("Listen 3000")
+        console.log ("Listen sss")
     }
 
 });
-=======
-  }
-);
+
 
 app.listen(3000);
->>>>>>> e034cf9ff92f6f29c77445602407e7703bcafedf
