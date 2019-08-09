942815b5de351c76bd9d477ff0e9590da0a1c66a
MrPotato98 <huy.potato1998@gmail.com>
2019-08-10 03:50:29 +0700

    main menu & listitem & table_dialog

---
+++ font-end/app/src/main/java/com/huypo/tase/Adapter/TableAdapter.java
@@ -0,0 +1,62 @@
+package com.huypo.tase.Adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.huypo.tase.Model.PersonalTable;
+import com.huypo.tase.R;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class TableAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<PersonalTable> personalTables;
+
+    public TableAdapter(Context context, ArrayList<PersonalTable> personalTables) {
+        this.context = context;
+        this.personalTables = personalTables;
+    }
+
+    @Override
+    public int getCount() {
+        return personalTables.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return personalTables.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView = View.inflate(context , R.layout.list_items, null);
+        }
+
+        ImageView images = convertView.findViewById(R.id.imageTable);
+        TextView tablename = convertView.findViewById(R.id.txtTableName);
+        TextView description = convertView.findViewById(R.id.txtTableDescription);
+        TextView deadline = convertView.findViewById(R.id.txtTableDeadline);
+
+        PersonalTable personalTable = personalTables.get(position);
+        images.setImageResource(personalTable.getImageTable());
+        tablename.setText(personalTable.getTxtTableName());
+        description.setText(personalTable.getTxtTableDescription());
+        deadline.setText(personalTable.getTxtTableDeadline().toString());
+
+        return convertView;
+    }
+}
